fix numerical issues

python_utils/external/pytorch3d_transformations.py
@@ -279,9 +279,9 @@ def matrix_to_euler_angles(matrix: torch.Tensor, convention: str) -> torch.Tenso
     i2 = _index_from_letter(convention[2])
     tait_bryan = i0 != i2
     if tait_bryan:
-        central_angle = torch.asin(matrix[..., i0, i2] * (-1.0 if i0 - i2 in [-1, 2] else 1.0))
+        central_angle = torch.asin(torch.clip(matrix[..., i0, i2] * (-1.0 if i0 - i2 in [-1, 2] else 1.0), -1.0, 1.0))
     else:
-        central_angle = torch.acos(matrix[..., i0, i0])
+        central_angle = torch.acos(torch.clip(matrix[..., i0, i0], -1.0, 1.0))
 
     o = (
         _angle_from_tan(convention[0], convention[1], matrix[..., i2], False, tait_bryan),